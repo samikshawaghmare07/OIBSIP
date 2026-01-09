20b47f60d8465f2f2d3e6e3629a6d8d18f7b4e1f
Samisha <johndoe@example.com>
2026-01-09 19:43:01 +0530

    first commit

--- webdevelopment_task1/script.js
+++ webdevelopment_task1/script.js
@@ -1,30 +1,24 @@
-// Get the display input box
 let display = document.getElementById("display");
 
-// Get all buttons
 let buttons = document.querySelectorAll("button");
 
-// Empty string to store user input
 let input = "";
 
 // Loop through each button
 for (let i = 0; i < buttons.length; i++) {
 
-    // Add click event to each button
     buttons[i].addEventListener("click", function () {
 
         let buttonText = buttons[i].innerText;
 
-        // If button is C → clear display
         if (buttonText === "C") {
             input = "";
             display.value = "";
         }
 
-        // If button is = → calculate result
         else if (buttonText === "=") {
             try {
-                input = eval(input);   // evaluate expression
+                input = eval(input);  
                 display.value = input;
             } catch {
                 display.value = "Error";
@@ -32,7 +26,7 @@ for (let i = 0; i < buttons.length; i++) {
             }
         }
 
-        // For numbers and operators
+        
         else {
             input = input + buttonText;
             display.value = input;
